5a58beb84ac63256db56a108a54d8a34eb04fa44
simonghales <simonghales@gmail.com>
2016-05-27 00:04:42 +1000

    editor

---
+++ src/app/components/core/box-editor/box-editor.directive.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('wordpreditor.core.directives')
+    .directive('boxEditor', boxEditor);
+
+  function boxEditor($log) {
+
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/core/box-editor/_box-editor.html',
+      replace: true,
+      scope: false
+    };
+
+    return directive;
+
+  }
+
+})();
